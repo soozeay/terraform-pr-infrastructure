e45faaf4e5894284b410a64ac4a4a55d4d6cde94
soozeay <takapi.921@gmail.com>
2023-12-11 22:42:01 +0900

    feat:php用のECR作成

--- admin-pr/envs/prod/app/foobar/ecr.tf
+++ admin-pr/envs/prod/app/foobar/ecr.tf
@@ -2,3 +2,8 @@ module "nginx" {
   source = "../../../../modules/ecr"
   name = "example-prod-foobar-nginx"
 }
+
+module "php" {
+  source = "../../../../modules/ecr"
+  name = "example-prod-foobar-php"
+}
