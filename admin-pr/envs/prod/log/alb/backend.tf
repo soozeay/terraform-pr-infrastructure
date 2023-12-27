70ef856ab607f2d5bfa959baef4e97765980e0d7
soozeay <takapi.921@gmail.com>
2023-12-28 00:13:37 +0900

    ドメイン取得とロードバランサーの作成

--- admin-pr/envs/prod/log/alb/backend.tf
+++ admin-pr/envs/prod/log/alb/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-training-tfstate"
-    key = "example/prod/network/main_v1.0.0.tfstate"
+    key = "example/prod/log/alb_v1.0.0.tfstate"
     region = "ap-northeast-1"
   }
 }
