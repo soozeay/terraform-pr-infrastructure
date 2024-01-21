c6c5b1343cc84b2d6509cadc736ea45604f9fe50
soozeay <takapi.921@gmail.com>
2024-01-22 01:13:24 +0900

    feat:rds構築

--- admin-pr/envs/prod/network/main/security_group.tf
+++ admin-pr/envs/prod/network/main/security_group.tf
@@ -50,3 +50,26 @@ resource "aws_security_group" "vpc" {
     Name = "${aws_vpc.this.tags.Name}-vpc"
   }
 }
+
+resource "aws_security_group" "db_foobar" {
+  name = "${aws_vpc.this.tags.Name}-db-foobar"
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    self = true
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${aws_vpc.this.tags.Name}-db-foobar"
+  }
+}
---
+++ admin-pr/envs/prod/network/main/shered_local.tf
@@ -0,0 +1,1 @@
+../../shered_local.tf
